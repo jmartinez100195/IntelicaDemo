4cf5f8ac202da7ad3c065958f11e851ba105ba26
Carlos Espejo <carlos.espejo@intelica.com>
2024-11-07 16:38:01 -0500

    Consulta de businessuserpage anulado

--- Intelica.Authentication.Backend/Intelica.Authentication.API/Domain/AuthenticationAggregate/Infrastructure/AuthenticationSQLServerRepository.cs
+++ Intelica.Authentication.Backend/Intelica.Authentication.API/Domain/AuthenticationAggregate/Infrastructure/AuthenticationSQLServerRepository.cs
@@ -6,7 +6,7 @@ namespace Intelica.Authentication.API.Domain.AuthenticationAggregate.Infrastruct
 {
     public class AuthenticationSQLServerRepository(Context context) : IAuthenticationRepository
     {
-        public void CreateAccessInformation(AccessInformation accessInformation) => context.AccessInformation.Add(accessInformation); 
+        public void CreateAccessInformation(AccessInformation accessInformation) => context.AccessInformation.Add(accessInformation);
         public AccessInformation? FindAccessInformation(Guid accessInformationID)
         {
             var row = context.AccessInformation.SingleOrDefault(x => x.AccessInformationID.Equals(accessInformationID));
@@ -18,7 +18,7 @@ namespace Intelica.Authentication.API.Domain.AuthenticationAggregate.Infrastruct
                         join profile in context.Profiles on businessUser.ProfileID equals profile.ProfileID
                         select new BussinesUserResponse(
                             businessUser.BusinessUserID, $"{businessUser.BusinessUserLastName}, {businessUser.BusinessUserName}",
-                            businessUser.BusinessUserEmail, businessUser.BusinessUserPassword, profile.ProfileName, businessUser.BusinessUserFirstLogin,null
+                            businessUser.BusinessUserEmail, businessUser.BusinessUserPassword, profile.ProfileName, businessUser.BusinessUserFirstLogin, null
                         );
             var list = query.ToList();
             if (list.Count == 0) return null;
@@ -31,9 +31,9 @@ namespace Intelica.Authentication.API.Domain.AuthenticationAggregate.Infrastruct
                         select new BussinesUserResponse(businessUser.BusinessUserID, $"{businessUser.BusinessUserLastName}, {businessUser.BusinessUserName}",
                         businessUser.BusinessUserEmail, businessUser.BusinessUserPassword, profile.ProfileName, businessUser.BusinessUserFirstLogin,
                         (
-                            from businessUserPage in context.BusinessUserPages.Where(x => x.BusinessUserID.Equals(businessUser.BusinessUserID))
-                            join page in context.Pages on businessUserPage.PageID equals page.PageID
-                            select new BusinessUserPageResponse(businessUserPage.PageID, page.PageRoot, businessUserPage.BusinessUserPageCanUpdate, 
+                            from businessUserPage in context.BusinessUserPages.Where(x => x.BusinessUserID.Equals(businessUser.BusinessUserID) && x.BusinessUserPageActive)
+                            join page in context.Pages on new { businessUserPage.PageID, PageActive = true } equals new { page.PageID, page.PageActive }
+                            select new BusinessUserPageResponse(businessUserPage.PageID, page.PageRoot, businessUserPage.BusinessUserPageCanUpdate,
                             businessUserPage.BusinessUserPageCanCreate, businessUserPage.BusinessUserPageCanDelete)
                         ).ToList()
                     );
